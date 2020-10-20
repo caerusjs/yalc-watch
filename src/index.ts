17d41e4cc6f5f785afcc606e81e3b7a53c8de91b
Kris Quigley <kris@krisquigley.co.uk>
2020-10-20 20:47:59 +0000

    Rebuild and published to use LF line endings

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-"use strict";
-import nodemon from "nodemon";
-import concurrently from "concurrently";
-import chalk from "chalk";
-import fs from "fs";
-import path from "path";
+'use strict';
+import nodemon from 'nodemon';
+import concurrently from 'concurrently';
+import chalk from 'chalk';
+import fs from 'fs';
+import path from 'path';
 
 interface YalcWatchConfig {
   watchFolder: string;
@@ -13,7 +13,9 @@ interface YalcWatchConfig {
 }
 
 // Get package.json contents
-const packageJson = JSON.parse(fs.readFileSync(path.join(process.cwd(), "package.json"), "utf8"));
+const packageJson = JSON.parse(
+  fs.readFileSync(path.join(process.cwd(), 'package.json'), 'utf8'),
+);
 
 // Get the yalcWatch section (if it exists)
 if (packageJson.yalcWatch) {
@@ -24,31 +26,37 @@ if (packageJson.yalcWatch) {
     yalcWatch.buildWatchCommand === undefined ||
     yalcWatch.extensions === undefined
   )
-    throw new Error('Invalid yalc watch config: "' + JSON.stringify(yalcWatch) + '"');
+    throw new Error(
+      'Invalid yalc watch config: "' + JSON.stringify(yalcWatch) + '"',
+    );
 
   nodemon({
     watch: [yalcWatch.watchFolder],
     ext: yalcWatch.extensions,
-    exec: "yalc push --changed"
+    exec: 'yalc push --changed',
     //delay: 1000
   });
 
   nodemon
-    .on("start", function() {
-      console.log(`${chalk.magentaBright("yalc-watch has started")}`);
+    .on('start', function () {
+      console.log(`${chalk.magentaBright('yalc-watch has started')}`);
     })
-    .on("quit", function() {
+    .on('quit', function () {
       //console.log("App has quit");
       process.exit();
     })
-    .on("restart", function(files: any) {
-      console.log(chalk.blueBright("Found changes in files:", chalk.magentaBright(files)));
-      console.log(chalk.blueBright("Trying to push new yalc package..."));
+    .on('restart', function (files: any) {
+      console.log(
+        chalk.blueBright('Found changes in files:', chalk.magentaBright(files)),
+      );
+      console.log(chalk.blueBright('Trying to push new yalc package...'));
     });
 
   concurrently([yalcWatch.buildWatchCommand]);
 } else {
   console.log(
-    chalk.redBright("Error: yalc-watch could not find a yalcWatch section in your package.json file, exiting")
+    chalk.redBright(
+      'Error: yalc-watch could not find a yalcWatch section in your package.json file, exiting',
+    ),
   );
 }
